7c62a1cfbc0d4d8606a1355f96e34e9d7639587c
salismazaya <salismazaya@gmail.com>
2026-01-19 22:34:06 +0700

    ✨ Add Glide library for image loading and implement category grid view
    🐛 Fix token retrieval logic and improve code structure in MainActivity
    
    📝 Update activity_main.xml layout for better UI structure and add category grid
    
    📝 Create and implement KategoriAdapter for displaying hotel categories
    
    🔧 Update build.gradle.kts to include Glide dependencies for image handling

--- app/src/main/java/com/unsera/hotel/MainActivity.java
+++ app/src/main/java/com/unsera/hotel/MainActivity.java
@@ -2,6 +2,7 @@ package com.unsera.hotel;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.GridView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +10,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.unsera.hotel.adapter.KategoriAdapter;
+import com.unsera.hotel.api.NinjaApiService;
+import com.unsera.hotel.api.RetrofitClient;
+import com.unsera.hotel.api.schemas.KategoriOut;
 import com.unsera.hotel.helpers.TokenManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +29,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         TokenManager tokenManager = new TokenManager(this);
-        if (tokenManager.getToken() == null) {
+        String token = tokenManager.getToken();
+        if (token == null) {
             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(intent);
             finish();
@@ -32,5 +45,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+//        KategoriAdapter kategoriAdapter = new KategoriAdapter(this, )
+//        String token = TokenManager()
+        NinjaApiService apiService = RetrofitClient.getApiService(token);
+
+        apiService.listKategori().enqueue(new Callback<List<KategoriOut>>() {
+            @Override
+            public void onResponse(Call<List<KategoriOut>> call, Response<List<KategoriOut>> response) {
+                if (response.isSuccessful()) {
+                    ArrayList<KategoriOut> kategoriOuts = (ArrayList<KategoriOut>) response.body();
+                    KategoriAdapter adapter = new KategoriAdapter(MainActivity.this, kategoriOuts);
+                    GridView gridView = findViewById(R.id.grid_view);
+                    gridView.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<KategoriOut>> call, Throwable t) {
+
+            }
+        });
     }
+
 }
--- app/src/main/java/com/unsera/hotel/adapter/KategoriAdapter.java
+++ app/src/main/java/com/unsera/hotel/adapter/KategoriAdapter.java
@@ -1,4 +1,47 @@
 package com.unsera.hotel.adapter;
 
-public class KategoriAdapter {
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.unsera.hotel.R;
+import com.unsera.hotel.api.schemas.KategoriOut;
+
+import java.util.ArrayList;
+public class KategoriAdapter extends ArrayAdapter<KategoriOut> {
+
+    public KategoriAdapter(Context context, ArrayList<KategoriOut> list) {
+        super(context, 0, list);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View itemView = convertView;
+        if (itemView == null) {
+            itemView = LayoutInflater.from(getContext()).inflate(R.layout.item_hotel_category, parent, false);
+        }
+
+        KategoriOut model = getItem(position);
+
+        TextView textView = itemView.findViewById(R.id.tvCategoryName);
+        ImageView imageView = itemView.findViewById(R.id.imgCategory);
+
+        if (model != null) {
+            textView.setText(model.nama);
+            Glide.with(itemView.getContext())
+                    .load("https://hotel.presensee.cloud/" + model.gambar)
+                    .placeholder(R.drawable.standard)
+                    .error(R.drawable.standard)
+                    .into(imageView);
+        }
+
+        return itemView;
+    }
 }
