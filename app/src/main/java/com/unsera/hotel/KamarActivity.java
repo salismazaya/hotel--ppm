80d0e266326bc33fd367ef93117a9713518e995c
salismazaya <salismazaya@gmail.com>
2026-01-25 19:46:55 +0700

    🐛 (KamarActivity.java): fix window insets listener by using correct layout ID 🔧 (activity_kamar.xml): add ID to root layout to enable proper insets handling

--- app/src/main/java/com/unsera/hotel/KamarActivity.java
+++ app/src/main/java/com/unsera/hotel/KamarActivity.java
@@ -15,7 +15,7 @@ public class KamarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_kamar);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layout_kamar), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
